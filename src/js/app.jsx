bad81dd4636da8933f485921c25a461b24a23013
Sean Harrell <seanvharrell@gmail.com>
2020-05-13 07:56:04 +0000

    updated UI and removed console logs

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,105 +2,101 @@ import React from 'react';
 
 export default class App extends React.Component {
   // your Javascript goes here
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
       balance: '',
       rate: '',
       term: 15,
-      monthlyPayment:'0',
+      monthlyPayment: '0',
     }
 
-    this.handleBalanceChange = this.handleBalanceChange.bind(this)
-    this.handleRateChange = this.handleRateChange.bind(this)
-    this.handleTermChange = this.handleTermChange.bind(this)
-    this.handleOnClick = this.handleOnClick.bind(this)
+    this.handleBalanceChange = this.handleBalanceChange.bind(this);
+    this.handleRateChange = this.handleRateChange.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
 
 
   }
 
-     handleOnClick (event){
-      //  console.log('this button was clicked')
+  handleOnClick(event) {
 
-      const rateConversion = this.state.rate/100/12
+    const rateConversion = this.state.rate / 100 / 12;
 
-      // console.log(rateConversion)
+    const monthlyRate = this.state.rate / 100 / 12 + 1;
 
-      const monthlyRate = this.state.rate/100/12 + 1
+    const totalMonthlyPayments = this.state.term * 12;
 
-      // console.log(monthlyRate)
+    const mortgageConversion = 1 - Math.pow(monthlyRate, -totalMonthlyPayments);
 
-      const totalMonthlyPayments = this.state.term * 12
+    const monthlyMortgagePayment = (rateConversion / mortgageConversion * this.state.balance).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 
-      // console.log(totalMonthlyPayments)
+    this.setState({
+      monthlyPayment: monthlyMortgagePayment
+    })
 
-      const mortgageConversion = 1 - Math.pow(monthlyRate,-totalMonthlyPayments)
-
-      // console.log (mortgageConversion)
-
-      const monthlyMortgagePayment = (rateConversion /mortgageConversion * this.state.balance).toFixed(2)
-      // console.log(monthlyMortgagePayment)
-
-      this.setState ({
-      monthlyPayment: monthlyMortgagePayment})
-
-      event.preventDefault();
-    }
+    event.preventDefault();
+  }
 
 
+  handleBalanceChange(event) {
+    this.setState({
+      balance: event.target.value
+    });
+  }
 
-  handleBalanceChange (event){
-      this.setState({
-        balance: event.target.value
-      })
-    }
-
-  handleRateChange (event){
+  handleRateChange(event) {
     this.setState({
       rate: event.target.value
-    })
+    });
   }
 
-  handleTermChange (event){
+  handleTermChange(event) {
     this.setState({
       term: event.target.value
-    })
+    });
   }
 
 
-    render() {
-        // console.log (this.state.balance,this.state.rate,this.state.term)
+  render() {
+    // console.log (this.state.balance,this.state.rate,this.state.term)
 
-      return (
-          <div className='container'>
-          {/* Insert JSX below */}
-            <h3>Mortgage Calculator</h3>
+    return (
+      <div className='container'>
+        {/* Insert JSX below */}
+        <h3 className=" display-4 text-center">Mortgage Calculator</h3>
 
-           <form className='form-horizontal' onSubmit={this.handleSubmit}>
-             <label> Loan Balance: </label>
-                <input name ='balance' type ='number'  placeholder='Loan Balance' value={this.state.balance} onChange={this.handleBalanceChange}/>
-                <br></br>
-
-               <label> Rate: </label>
-                <input name='rate' type ='number' step ='0.01' placeholder='APR %'value ={this.state.rate} onChange={this.handleRateChange}/>
-                <br></br>
-
-              <label>Term: </label>
-                <select name='term' value={this.state.term} onChange={this.handleTermChange}>
-                 <option value =''></option>
-                 <option value='15'> 15 </option>
-                 <option value='30'> 30 </option>
-                </select>
-                <br></br>
-
-            <button name='submit' onClick={this.handleOnClick} > Calculate </button>
-              <br></br>
-              <br></br>
-
-            <div name ='output' id='output' value={this.state.monthlyPayment}>${this.state.monthlyPayment} is your monthly payment.</div>
-            </form>
+        <form>
+          <label className='lead'>Loan Balance:</label>
+          <div className='input-group mb-3'>
+            <div className="input-group-prepend">
+              <span className="input-group-text">$</span>
+            </div>
+            <input name='balance' type='text' className='form-control' value={this.state.balance} onChange={this.handleBalanceChange} />
           </div>
-      );
-    }
+          <label className='lead'>Interest Rate:</label>
+          <div className='input-group mb-3'>
+            <div className="input-group-prepend">
+              <span className="input-group-text">%</span>
+            </div>
+            <input name='rate' type='text' className='form-control' step='1.0' value={this.state.rate} onChange={this.handleRateChange} />
+          </div>
+          <div className='form-group'>
+            <label className='lead'>Loan Term:</label>
+            <select className='form-control' name='term' value={this.state.term} onChange={this.handleTermChange}>
+              <option value=''></option>
+              <option value='15'>15 years</option>
+              <option value='30'>30 years</option>
+            </select>
+          </div>
+          <button className='btn btn-dark btn-lg btn-block' type='button' name='submit' onClick={this.handleOnClick} > Calculate </button>
+          <br></br>
+          <br></br>
+
+          <div className='lead text-center' name='output' id='output' value={this.state.monthlyPayment}> Your estimated monthly payment is ${this.state.monthlyPayment}.</div>
+        </form>
+      </div>
+    );
+  }
 }
